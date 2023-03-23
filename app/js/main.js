08e12260c7391f5e8da834c9612e778a8902950d
first commit <stebataras@gmail.com>
2023-03-24 00:36:27 +0100

    section slider completed

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,39 @@
-const box = document.querySelector('.box');
-const btn = document.querySelector('.button');
+
+// let answer = confirm('Do you have 18 years old ?');
+// if (answer==true) {
+//     console.log('Com on')
+// } else {
+//     console.log('Stop')
+// }
+
+
+// for (i=1; i<8; i++) {
+//     console.log(i)
+// }
+
+
+// function logging (a, b) {
+//     console.log(a + b)
+// }
+// logging(3, 6);
+
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../images/carousel/arrow-prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../images/carousel/arrow-next.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                dots: true,
+                arrows: false
+                }
+            }
+            
+        ]
+      });
+});
