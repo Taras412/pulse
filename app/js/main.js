6817c9954438a60a5f14a7ea49d37c6345590aec
first commit <stebataras@gmail.com>
2023-05-11 21:15:44 +0200

    Validation form is successfull (JS, with out jquerry)

--- app/js/main.js
+++ app/js/main.js
@@ -1,25 +1,4 @@
 
-// let answer = confirm('Do you have 18 years old ?');
-// if (answer==true) {
-//     console.log('Com on')
-// } else {
-//     console.log('Stop')
-// }
-
-
-// for (i=1; i<8; i++) {
-//     console.log(i)
-// }
-
-
-// function logging (a, b) {
-//     console.log(a + b)
-// }
-// logging(3, 6);
-
-
-
-
 
 $(document).ready(function(){
     $('.carousel__inner').slick({
@@ -81,5 +60,71 @@ $(document).ready(function(){
                              
 });
 
+//TEST FORMS VALIDATION
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function() {
+    const form = document.getElementById('add-form');
+
+    form.addEventListener('submit', formSend);
+    async function formSend(e) {
+        e.preventDefault();
+
+        let error = formValidate(form);
+        if (error === 0) {
+            let response = await fetch('sendmail.php', {
+                method: 'POST',
+                body:formData
+            });
+            if (response.ok) {
+
+            }else {}
+        }else {
+            alert('uncorrect!');
+        }
+    }
+
+
+    function formValidate(form) {
+        let error = 0;
+        let formReq = document.querySelectorAll('._req');
+
+        for (let index = 0; index < formReq.length; index++) {
+            const input = formReq[index];
+            formRemoveError(input);
+
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
+                    formAddError(input);
+                    error++;
+                }
+            }else if(input.getAttribute("type") === "checkbox" && input.checked === false){
+                formAddError(input);
+                error++;
+            }else {
+                if (input.value === '') {
+                    formAddError(input);
+                    error++;
+                }
+            }
+
+        }
+        return error;
+    }
+
+
+    function formAddError(input) {
+        input.parentElement.classList.add('_error');
+        input.classList.add('error');
+    }
+    function formRemoveError(input) {
+        input.parentElement.classList.remove('_error');
+        input.classList.remove('error');
+    }
+    //test email
+    function emailTest (input) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+    }
+});
 
 
