29051e26dfad14d349ebd51081b4ac44b03368f5
first commit <stebataras@gmail.com>
2023-05-11 19:41:27 +0200

    test forms

--- app/js/main.js
+++ app/js/main.js
@@ -77,43 +77,9 @@ $(document).ready(function(){
             $('.overlay, #order').fadeIn('slow');
         })
     });
-
-    /* $('#consultation-form').validate();  ( error jqerry validate )
-    $('#consultation form').validate();
-    $('#order form').validate(); */
-
-    /* $('input[name=phone]').mask("+7 (099) 999-99-99");      ( error tel mask )                         */     
+               
+                             
 });
 
 
 
-// VALIDATION FORM JS (test)
-
-function validation(form) {
-
-    function createError(input, text) {
-        const parent = input.parentNode;
-        console.log(parent);
-    }
-
-    let result = true;
-
-    const allInputs = form.querySelectorAll('input');
-    
-    for (const input of allInputs) {
-        if(input.value == "") {
-            console.log('Error !');
-            result = false
-        }
-    }
-    return result
-}
-
-
-document.getElementById('add-form').addEventListener('submit', function(event) {
-    event.preventDefault()
-    
-    if(validation(this) == true) {
-        alert('form checked successful !')
-    }
-})
