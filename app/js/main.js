59ba985c96b79934047a3b91b2ef3b097ae665c3
first commit <stebataras@gmail.com>
2023-05-11 23:27:09 +0200

    VALIDATION form is OK! Modal Window - NO! Sending mail must be finalize!

--- app/js/main.js
+++ app/js/main.js
@@ -65,20 +65,27 @@ $(document).ready(function(){
 
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('add-form');
-
     form.addEventListener('submit', formSend);
+
     async function formSend(e) {
         e.preventDefault();
 
         let error = formValidate(form);
         if (error === 0) {
+            form.classList.add('_sending');
             let response = await fetch('sendmail.php', {
                 method: 'POST',
                 body:formData
             });
             if (response.ok) {
-
-            }else {}
+                let result = await response.json();
+                alert(result.message);
+                formPreview.innerHTML = '';
+                form.reset();
+                form.classList.remowe('_sending');
+            }else {
+                alert('uncorrect!');
+            }
         }else {
             alert('uncorrect!');
         }
--- app/mailer/smart.php
+++ app/mailer/smart.php
@@ -1,44 +1,45 @@
+
 <?php 
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
 
-$name = $_POST['user-name'];
-$phone = $_POST['user-phone'];
-$email = $_POST['user-email'];
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/Exception.php';
 
-require_once('phpmailer/PHPMailerAutoload.php');
-$mail = new PHPMailer;
-$mail->CharSet = 'utf-8';
+	$mail = new PHPMailer(true);
+	$mail ->CharSet = 'UTF-8';
+	$mail ->setLanguage('ru', 'phpmailer/language/');
+	$mail ->IsHTML(true);
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
+	//mail from:
+	$mail ->setFrom('info@fls.guru', 'This is my name');
+	//mail to:
+	$mail ->addAdress('stebataras@gmail.com');
+	//mail about:
+	$mail ->Subject = 'Hi , This is my first mail';
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'stebataras@gmail.com';                 // Наш логин
-$mail->Password = 'Apokalipsis12';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
- 
-$mail->setFrom('stebataras@gmail.com', 'Pulse');   // От кого письмо 
-$mail->addAddress('stebataras@gmail.com');     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+	//body's mail:
+	$body = '<h1>Congratulation, Great mail!</h1>';
 
-$mail->Subject = 'Данные';
-$mail->Body    = '
-		Пользователь оставил данные <br> 
-	Имя: ' . $name . ' <br>
-	Номер телефона: ' . $phone . '<br>
-	E-mail: ' . $email . '';
+	if(trim(!empty($_POST['name']))){
+		$body = '<p><strong>Name:</strong> '.$_POST['name'].'</p>';
+	}
+	if(trim(!empty($_POST['email']))){
+		$body = '<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+	if(trim(!empty($_POST['message']))){
+		$body = '<p><strong>Message:</strong> '.$_POST['message'].'</p>';
+	}
+	//sending mail:
+	if(!$mail->send()) {
+		$message = 'Error!'
+	}else {
+		$message = 'Data is sending!'
+	}
 
-if(!$mail->send()) {
-    return false;
-} else {
-    return true;
-}
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo Json_encode($response);
 
 ?>
